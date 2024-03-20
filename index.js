133cf22a52762d9526fd6be392cc66ac6bb2256a
Mohamed Ahmed <ma07041705@gmail.com>
2024-03-20 15:23:30 +0300

    Destructuring Assignment

--- index.js
+++ index.js
@@ -56,9 +56,7 @@ const { muppetName, color, song, job, partner } = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const {
-  album: {
-    theMuppetMovie: { song2, song4 }
-  },
+  album: { theMuppetMovie: { song2, song4 }},
   nestedJob,
   nestedPartner
 } = nestedMuppet;
